4262de4c237a31221e95276060411b5297da0af9
Edward Vidal Jr <develone@sbcglobal.net>
2025-02-05 10:44:52 -0700

    devel@pi5-90:~/master-repos/pico-pico2-freertos $ git diff openjpeg/openjpeg-lib/dwtlift.h diff --git a/openjpeg/openjpeg-lib/dwtlift.h b/openjpeg/openjpeg-lib/dwtlift.h index ec1ca06..13e35a2 100644 --- a/openjpeg/openjpeg-lib/dwtlift.h +++ b/openjpeg/openjpeg-lib/dwtlift.h @@ -9,7 +9,7 @@ int loop, decomp, encode;  double sqrt(double x);
     void lift_config(int dec, int enc, int TCP_DISTORATIO, int FILTER,  int CR, int flg, int bp, long imgsz,long him,long wim, int *bufferptr);
    -
    +int octave_write_byte(const char * fn,char * d_ptr, int sz);
     int x0, yy0,  x1,  yy1;
     char *ff_in;

--- openjpeg/openjpeg-lib/dwtlift.h
+++ openjpeg/openjpeg-lib/dwtlift.h
@@ -9,7 +9,7 @@ int loop, decomp, encode;
 double sqrt(double x);
  
 void lift_config(int dec, int enc, int TCP_DISTORATIO, int FILTER,  int CR, int flg, int bp, long imgsz,long him,long wim, int *bufferptr);
-
+int octave_write_byte(const char * fn,char * d_ptr, int sz);
 int x0, yy0,  x1,  yy1;
 char *ff_in; 
 
